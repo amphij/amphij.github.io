b376ec3ea056a3c03b6564de71ae05a074939981
amphij <aman.amphij@gmail.com>
2024-08-12 14:57:03 +0530

    add amphij ui code

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2707
+\cocoatextscaling0\cocoaplatform0{\fonttbl}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+}
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if ( scroll >= 10) {
+        $(".navbar").addClass("nav-scroll");
+        $(".back_to_top").addClass("show_btn");
+    } else {
+        $(".navbar").removeClass("nav-scroll");
+        $(".back_to_top").removeClass("show_btn");
+    }
+});
+
+font-family: 'Poppins', sans-serif;
+font-family: 'Raleway', sans-serif;
+font-family: 'Open Sans', sans-serif;
+
+
+
+
+
+
+
+
+
+
